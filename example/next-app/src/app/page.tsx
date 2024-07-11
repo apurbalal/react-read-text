851b528fcd4d24d0cf1ce7cf41984062ec2e00f8
Apurbalal Singha <apurbalal@mercari.com>
2024-07-12 06:08:31 +0900

    Update demo

--- example/next-app/src/app/page.tsx
+++ example/next-app/src/app/page.tsx
@@ -13,32 +13,68 @@ const DynamicReadingController = dynamic(
 );
 
 const markdown = `
-  # A demo of \`react-markdown\`
+An h1 header
+============
 
-  \`react-markdown\` is a markdown component for React.
+Paragraphs are separated by a blank line.
 
-  👉 Changes are re-rendered as you type.
+2nd paragraph. *Italic*, **bold**, and \`monospace\`. Itemized lists
+look like:
 
-  👈 Try writing some markdown on the left.
+  * this one
+  * that one
+  * the other one
 
-  ## Overview
+Note that --- not considering the asterisk --- the actual text
+content starts at 4-columns in.
 
-  * Follows [CommonMark](https://commonmark.org)
-  * Optionally follows [GitHub Flavored Markdown](https://github.github.com/gfm/)
-  * Renders actual React elements instead of using \`dangerouslySetInnerHTML\`
-  * Lets you define your own components (to render \`MyHeading\` instead of \`'h1'\`)
-  * Has a lot of plugins
+> Block quotes are
+> written like so.
+>
+> They can span multiple paragraphs,
+> if you like.
 
-  ## Contents
+Use 3 dashes for an em-dash. Use 2 dashes for ranges (ex., "it's all
+in chapters 12--14"). Three dots ... will be converted to an ellipsis.
+Unicode is supported. ☺
 
-  Here is an example of a plugin in action
-  ([\`remark-toc\`](https://github.com/remarkjs/remark-toc)).
-  **This section is replaced by an actual table of contents**.
 
-  ## Syntax highlighting
 
-  Here is an example of a plugin to highlight code:
-  [\`rehype - highlight\`](https://github.com/rehypejs/rehype-highlight).
+An h2 header
+------------
+
+Here's a numbered list:
+
+ 1. first item
+ 2. second item
+ 3. third item
+
+Note again how the actual text starts at 4 columns in (4 characters
+from the left side). Here's a code sample:
+
+    # Let me re-iterate ...
+    for i in 1 .. 10 { do-something(i) }
+
+As you probably guessed, indented 4 spaces. By the way, instead of
+indenting the block, you can use delimited blocks, if you like:
+
+~~~
+define foobar() {
+    print "Welcome to flavor country!";
+}
+~~~
+
+(which makes copying & pasting easier). You can optionally mark the
+delimited block for Pandoc to syntax highlight it:
+
+~~~python
+import time
+# Quick, count to ten!
+for i in range(10):
+    # (but not *too* quick)
+    time.sleep(0.5)
+    print i
+~~~
 `;
 
 export default function Home() {
